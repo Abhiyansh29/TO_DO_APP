6d9f2cb451fee81718f0b429f02a172cdd11943e
Abhi <abhiyanshvarshney9@gmail.com>
2026-04-02 14:27:13 +0530

    [Abhiyansh]. commit  Middleware Update

--- backend/src/controllers/todo.controller.js
+++ backend/src/controllers/todo.controller.js
@@ -2,6 +2,7 @@ const Todo=require("../models/todo.model")
 const createTodo=async(req,res)=>{
     try{
         const todo=await Todo.create(req.body)
+        res.status(200).json({data:todo})
     }
     catch(error){
         res.status(500).json({"error":error.message})
